53721df5e2fa6de4e78bd1325633f9122aa2bc83
DaryaV@munich.quantag-it.com <darya.vyshnyakova@quantag-it.com>
2019-06-07 17:33:37 +0300

    Updates

---
+++ precache-manifest.8b265741c8a6c717ae87347803d820d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120021dc35e7aab43600e9ad377b64b1",
+    "url": "/fe_on_feb19-phones-react/index.html"
+  },
+  {
+    "revision": "6d7ac2247f4c4fd7c227",
+    "url": "/fe_on_feb19-phones-react/static/css/main.95ac9038.chunk.css"
+  },
+  {
+    "revision": "d27dcead68faee6d2b37",
+    "url": "/fe_on_feb19-phones-react/static/js/2.44949046.chunk.js"
+  },
+  {
+    "revision": "6d7ac2247f4c4fd7c227",
+    "url": "/fe_on_feb19-phones-react/static/js/main.b552fb0c.chunk.js"
+  },
+  {
+    "revision": "13ef023f66a5d3f30d63",
+    "url": "/fe_on_feb19-phones-react/static/js/runtime~main.6b2147bd.js"
+  }
+]);
